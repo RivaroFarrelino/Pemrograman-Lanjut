cba961713f728fc730d61bcf719039542dde60ed
RivaroFarrelino <rivfarrelino@gmail.com>
2023-05-11 12:51:30 +0700

    update biar kayak test case modul

--- TugasPemlan/Praktikum5/Main.java
+++ TugasPemlan/Praktikum5/Main.java
@@ -2,16 +2,34 @@ package TugasPemlan.Praktikum5;
 
 public class Main {
     public static void main(String[] args) {
-        Manusia a = new Manusia("A", "111", true, true);
-        System.out.println(a);
+        Manusia manusia1 = new Manusia("A", "123", true, true);
+        System.out.println(manusia1);
 
-        MahasiswaFILKOM b = new MahasiswaFILKOM("165150300111100", 4.0, "B", "111", false, false);
-        System.out.println(b);
+        Manusia manusia2 = new Manusia("B", "456", false, true);
+        System.out.println(manusia2);
 
-        Pekerja c = new Pekerja(1000, 2016, 3, 2, 4, "C", "111", true, true);
-        System.out.println(c);
+        Manusia manusia3 = new Manusia("C", "789", true, false);
+        System.out.println(manusia3);
 
-        Manager d = new Manager("HRD", 1000, 2017, 1, 2, 3, "D", "111", true, true);
-        System.out.println(d);
+        MahasiswaFILKOM mahasiswaFILKOM1 = new MahasiswaFILKOM("225150707111076", 4, "Muhammad Rivaro Farrelino Gozali", "3175073006", true, false);
+        System.out.println(mahasiswaFILKOM1);
+        
+        MahasiswaFILKOM mahasiswaFILKOM2 = new MahasiswaFILKOM("225150707111076", 3.7, "Muhammad Rivaro Farrelino Gozali", "3175073006", true, false);
+        System.out.println(mahasiswaFILKOM2);
+        
+        MahasiswaFILKOM mahasiswaFILKOM3 = new MahasiswaFILKOM("225150707111076", 2.8, "Muhammad Rivaro Farrelino Gozali", "3175073006", true, false);
+        System.out.println(mahasiswaFILKOM3);
+
+        Pekerja pekerja1 = new Pekerja(5000, 2021, 5, 10, 2, "D", "34567", true, true);
+        System.out.println(pekerja1);
+        
+        Pekerja pekerja2 = new Pekerja(6000, 2014, 5, 10, 0, "E", "31892", false, false);
+        System.out.println(pekerja2);
+
+        Pekerja pekerja3 = new Pekerja(6200, 2003, 5, 10, 10, "F", "85212", true, true);
+        System.out.println(pekerja3);
+
+        Manager manager1 = new Manager("HRD", 7500, 2008, 5, 10, 0, "G", "678910", true, false);
+        System.out.println(manager1);
     }
 }
